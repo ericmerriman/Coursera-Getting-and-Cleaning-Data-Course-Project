ed4f42d6fc74885933adf77d2f13f83d301ce9eb
ntnntn <antonio.intini@gmail.com>
2014-12-21 17:11:02 +0100

    Final commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Getting and Cleaning Data - Course Project
+==========================================
+
+This repository includes the R code and documentation files for the Data Science's Coursera course "Getting and Cleaning data”.
+
+The dataset used is “Human Activity Recognition Using Smartphones” available at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones.
+
+The code assumes that all the data is present in the UCI HAR Dataset folder, un-compressed and placed in the working directory.
+
+“CodeBook.md” describes the variables, the data, and any transformations performed to clean up the data.	
+
+“run_analysis.R” is the R script that performs the analyses through the 5 steps described in the CodeBook.m generating the output file called “tidy_data.txt”.
---
+++ run_analysis.R
@@ -0,0 +1,69 @@
+# Assuming that the Samsung data is in his own UCI HAR Dataset directory, placed in the working directory
+# The script require the plyr package
+library(plyr)
+
+# Merge the training and test sets to create one data set
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# create x,y,subject data set
+x_data <- rbind(x_train, x_test)
+y_data <- rbind(y_train, y_test)
+subject_data <- rbind(subject_train, subject_test)
+
+# Extract only the measurements on the mean and standard deviation for each measurement
+features <- read.table("UCI HAR Dataset/features.txt")
+
+# get only columns with mean() or std() in their names
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+# subset the desired columns
+x_data <- x_data[, mean_and_std_features]
+
+# correct the column names
+names(x_data) <- features[mean_and_std_features, 2]
+
+# Use descriptive activity names to name the activities in the data set
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name
+names(y_data) <- "activity"
+
+# Appropriately label the data set with descriptive variable names
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
+colNames <- colnames(all_data)
+
+# Appropriately label the data set with descriptive activity names 
+for (i in 1:length(colNames)) 
+{
+        colNames[i] = gsub("\\()","",colNames[i])
+        colNames[i] = gsub("-std$","StdDev",colNames[i])
+        colNames[i] = gsub("-mean","Mean",colNames[i])
+        colNames[i] = gsub("^(t)","time",colNames[i])
+        colNames[i] = gsub("^(f)","freq",colNames[i])
+        colNames[i] = gsub("([Gg]ravity)","Gravity",colNames[i])
+        colNames[i] = gsub("([Bb]ody[Bb]ody|[Bb]ody)","Body",colNames[i])
+        colNames[i] = gsub("[Gg]yro","Gyro",colNames[i])
+        colNames[i] = gsub("AccMag","AccMagnitude",colNames[i])
+        colNames[i] = gsub("([Bb]odyaccjerkmag)","BodyAccJerkMagnitude",colNames[i])
+        colNames[i] = gsub("JerkMag","JerkMagnitude",colNames[i])
+        colNames[i] = gsub("GyroMag","GyroMagnitude",colNames[i])
+}
+colnames(all_data) <- colNames
+
+# Create a second, independent tidy data set with the average of each variable for each activity and each subject
+tidy_data <- ddply(all_data, .(subject, activity), numcolwise(mean))
+
+write.table(tidy_data, "tidy_data.txt", row.name=FALSE)
